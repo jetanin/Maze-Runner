0c330299f02aa3f16d42ddca6e1cb5259e62c737
Jetanin Naitho <97624409+jetanin@users.noreply.github.com>
2025-12-13 16:24:47 +0700

    Modify run.bat to include exit command
    Add exit command after running the Java application.

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 javac -d bin -sourcepath src .\src\th\ac\kmutt\cpe\algorithm\maze\*.java
-java -cp bin th.ac.kmutt.cpe.algorithm.maze.Main
 echo running Maze Runner...
-pause
+java -cp bin th.ac.kmutt.cpe.algorithm.maze.Main
+exit
